0d7a03ba1c97f3ae3d25c33dfd7e2b68249a1d8c
Giulio Procopio <giulioprocopioo@gmail.com>
2024-04-06 18:42:25 +0200

    Fix GLFW include path

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
