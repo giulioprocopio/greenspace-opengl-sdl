1e5a2ce9ec51106400093eb1d423b917161d2ae3
Giulio Procopio <giulioprocopioo@gmail.com>
2024-04-07 15:46:23 +0200

    Add Apple macro forward compat check

--- src/main.cc
+++ src/main.cc
@@ -13,14 +13,19 @@ void process_input(GLFWwindow *window) {
   }
 }
 
+const unsigned SCR_WIDTH = 800, SCR_HEIGHT = 600;
+
 int main(void) {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Greenspace", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Greenspace", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
